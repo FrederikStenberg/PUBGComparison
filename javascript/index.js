545a5861261cd7439d57be5473c549392cc5cfbd
Frederik Pedersen <frecodev@gmail.com>
2018-04-25 12:36:49 +0200

    Landing page finish

--- javascript/index.js
+++ javascript/index.js
@@ -1,34 +1,16 @@
-$(document).ready(function() {
-    
-    var selectedRegion; 
-    var player1;
-    var player2;
-    
-    $(document).keypress(function(e) {
-    var keycode = (e.keyCode ? e.keyCode : e.which);
-        if(keycode == '13') { 
-            if($('#option1').is(':checked')) {
-                selectedRegion = "pc-eu";
-            } else if ($('#option2').is(':checked')) {
-                selectedRegion = "pc-krjp";
-            } else if ($('#option3').is(':checked')) {
-                selectedRegion = "pc-na";
-            } else if ($('#option4').is(':checked')) {
-                selectedRegion = "pc-oc";
-            } else if ($('#option5').is(':checked')) {
-                selectedRegion = "pc-kakao";
-            } else if ($('#option6').is(':checked')) {
-                selectedRegion = "pc-sea";
-            } else if ($('#option7').is(':checked')) {
-                selectedRegion = "pc-sa";
-            } else if ($('#option8').is(':checked')) {
-                selectedRegion = "pc-as";
-            }
-            var player1 = document.getElementById('searchplayer1').value;
-            var player2 = document.getElementById('searchplayer2').value;
-        }
-    });
-});
+var player1;
+var player2;
+var region1;
+var region2;
+
+function compare() {
+    var regionElement1 = document.getElementById('regionplayer1');
+    var regionElement2 = document.getElementById('regionplayer2');
+    player1 = document.getElementById('searchplayer1').value;
+    player2 = document.getElementById('searchplayer2').value;
+    region1 = regionElement1.options[regionElement1.selectedIndex].value;
+    region2 = regionElement2.options[regionElement2.selectedIndex].value;
+};
 
 /* JS for dropdown */
 var colors = ['1abc9c', '2c3e50', '2980b9', '7f8c8d', 'f1c40f', 'd35400', '27ae60'];
