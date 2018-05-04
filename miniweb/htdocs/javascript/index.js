d10ef387ca338c310b7c10c7216a405323935107
Frederik Pedersen <frecodev@gmail.com>
2018-05-04 11:20:23 +0200

    kk

--- miniweb/htdocs/javascript/index.js
+++ miniweb/htdocs/javascript/index.js
@@ -1,5 +1,7 @@
 var player1;
 var player2;
+var player1ID;
+var player2ID;
 var region1;
 var region2;
 
@@ -13,19 +15,20 @@ function compare() {
     
     var req = new XMLHttpRequest();
     var req2 = new XMLHttpRequest();
-    var url = "https://api.playbattlegrounds.com/shards/" + region1 + "/players?filter[playerNames]=" + player1;
+    //var url = "https://api.playbattlegrounds.com/shards/" + region1 + "/players?filter[playerNames]=" + player1;
+    var url = "https://api.playbattlegrounds.com/shards/" + region1 + "/players/" + player1ID + "/seasons/division.bro.official.2018-04";
     var url2; "https://api.playbattlegrounds.com/shards/" + region2 + "/players?filter[playerNames]=" + player2;
 
     req.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             var obj = this.responseText;
             var objJSON = JSON.parse(obj);
-            var amountOfMatches = objJSON.data["0"].relationships.matches.data.length;
-            sessionStorage.setItem("amountOfMatches", amountOfMatches);
+            //var amountOfMatches = objJSON.data["0"].relationships.matches.data.length;
+            //sessionStorage.setItem("amountOfMatches", amountOfMatches);
         }
     }
     
-    req2.onreadystatechange = function() {
+    /*req2.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             var obj2 = this.responseText;
             var obj2JSON = JSON.parse(obj2);
@@ -33,15 +36,15 @@ function compare() {
             sessionStorage.setItem("amountOfMatches2", amountOfMatches2);
             window.location.href = "selectmatch.html";
         }
-    }
+    }*/
     
     req.open('GET', url, true);
     req.setRequestHeader('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5Mjk1YWExMC0yYWIxLTAxMzYtZTNhNi0wOTkxMzhmZmQ5ZTEiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTI0NjU5MDg5LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1YmctY29tcGFyaXNvbiIsInNjb3BlIjoiY29tbXVuaXR5IiwibGltaXQiOjEwfQ.45-CAzKJIChq3z7h-9He_dUTFOvDHzDesnvWOcS719w');
     req.setRequestHeader('Accept', 'application/vnd.api+json');
     req.send();
     
-    req2.open('GET', url2, true);
+    /*req2.open('GET', url2, true);
     req2.setRequestHeader('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5Mjk1YWExMC0yYWIxLTAxMzYtZTNhNi0wOTkxMzhmZmQ5ZTEiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTI0NjU5MDg5LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1YmctY29tcGFyaXNvbiIsInNjb3BlIjoiY29tbXVuaXR5IiwibGltaXQiOjEwfQ.45-CAzKJIChq3z7h-9He_dUTFOvDHzDesnvWOcS719w');
     req2.setRequestHeader('Accept', 'application/vnd.api+json');
-    req2.send();
+    req2.send();*/
 };
