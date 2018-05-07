82a43afb9e02df747835c0a6c66a8f96c7a5fe23
Frederik Pedersen <frecodev@gmail.com>
2018-05-07 14:30:44 +0200

    Somewhat acceptable version

--- miniweb/htdocs/javascript/comparison.js
+++ miniweb/htdocs/javascript/comparison.js
@@ -1,3 +1,22 @@
-function test() {
-    alert(sessionStorage.getItem("amountOfMatches"));
+window.onload = function() {
+    document.getElementById("PlayerName1Session").innerHTML = sessionStorage.getItem("playerName1");
+    document.getElementById("PlayerName2Session").innerHTML = sessionStorage.getItem("playerName2");
+
+    document.getElementById("player1Kills").innerHTML = sessionStorage.getItem("player1Kills");
+    document.getElementById("player1Assists").innerHTML = sessionStorage.getItem("player1Assists");
+    document.getElementById("player1WinRatio").innerHTML = sessionStorage.getItem("player1WinRatio") + "%";
+    document.getElementById("player1MostKills").innerHTML = sessionStorage.getItem("player1RoundMostKills");
+    document.getElementById("player1Suicides").innerHTML = sessionStorage.getItem("player1Suicides");
+    document.getElementById("player1Teamkills").innerHTML = sessionStorage.getItem("player1Teamkills");
+    document.getElementById("player1Top10s").innerHTML = sessionStorage.getItem("player1Top10s");
+    document.getElementById("player1LongestKill").innerHTML = sessionStorage.getItem("player1LongestKill") + "m";
+
+    document.getElementById("player2Kills").innerHTML = sessionStorage.getItem("player2Kills");
+    document.getElementById("player2Assists").innerHTML = sessionStorage.getItem("player2Assists");
+    document.getElementById("player2WinRatio").innerHTML = sessionStorage.getItem("player2WinRatio") + "%";
+    document.getElementById("player2MostKills").innerHTML = sessionStorage.getItem("player2RoundMostKills");
+    document.getElementById("player2Suicides").innerHTML = sessionStorage.getItem("player2Suicides");
+    document.getElementById("player2Teamkills").innerHTML = sessionStorage.getItem("player2Teamkills");
+    document.getElementById("player2Top10s").innerHTML = sessionStorage.getItem("player2Top10s");
+    document.getElementById("player2LongestKill").innerHTML = sessionStorage.getItem("player2LongestKill") + "m";
 };
--- miniweb/htdocs/javascript/index.js
+++ miniweb/htdocs/javascript/index.js
@@ -21,6 +21,9 @@ function compare() {
     var urlForPlayer1ID = "https://api.playbattlegrounds.com/shards/" + region1 + "/players?filter[playerNames]=" + player1;
     var urlForPlayer2ID = "https://api.playbattlegrounds.com/shards/" + region2 + "/players?filter[playerNames]=" + player2;
     
+    sessionStorage.setItem("playerName1", player1);
+    sessionStorage.setItem("playerName2", player2);
+    
     //Getting info for Player 1
     req.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
@@ -29,7 +32,7 @@ function compare() {
             player1ID = objJSON.data["0"].id;
             
             //Begin second API call to get player1 data
-            var urlForSeasonObject1 = "https://api.playbattlegrounds.com/shards/" + region1 + "/players/" + player1ID + "/seasons/division.bro.official.2018-04";
+            var urlForSeasonObject1 = "https://api.playbattlegrounds.com/shards/" + region1 + "/players/" + player1ID + "/seasons/division.bro.official.2018-05";
             req2.open('GET', urlForSeasonObject1, true);
             req2.setRequestHeader('Authorization', 'Bearer ' + APIkey);
             req2.setRequestHeader('Accept', 'application/vnd.api+json');
@@ -46,13 +49,15 @@ function compare() {
             player1RoundMostKills = obj2JSON.data.attributes.gameModeStats["duo-fpp"].roundMostKills;
             player1Suicides = obj2JSON.data.attributes.gameModeStats["duo-fpp"].suicides;
             player1Top10s = obj2JSON.data.attributes.gameModeStats["duo-fpp"].top10s;
-            player1LongestKill = obj2JSON.data.attributes.gameModeStats["duo-fpp"].longestKill;
+            player1LongestKillString = obj2JSON.data.attributes.gameModeStats["duo-fpp"].longestKill;
+            player1LongestKill = parseFloat(player1LongestKillString).toFixed(2);
             player1Teamkills = obj2JSON.data.attributes.gameModeStats["duo-fpp"].teamkills;
             
             //Calculate winratio
             player1Wins = obj2JSON.data.attributes.gameModeStats["duo-fpp"].wins;
             player1Rounds = obj2JSON.data.attributes.gameModeStats["duo-fpp"].roundsPlayed;
-            player1WinRatio = (player1Wins / player1Rounds) * 100;
+            player1WinRatio = ((player1Wins / player1Rounds) * 100).toFixed(2);
+            
             
             //Put variables into storage
             sessionStorage.setItem("player1Kills", player1Kills);
@@ -78,7 +83,7 @@ function compare() {
             player2ID = obj3JSON.data["0"].id;
                                 
             //Begin second API call to get player2 data
-            var urlForSeasonObject1 = "https://api.playbattlegrounds.com/shards/" + region2 + "/players/" + player2ID + "/seasons/division.bro.official.2018-04";
+            var urlForSeasonObject1 = "https://api.playbattlegrounds.com/shards/" + region2 + "/players/" + player2ID + "/seasons/division.bro.official.2018-05";
             req4.open('GET', urlForSeasonObject1, true);
             req4.setRequestHeader('Authorization', 'Bearer ' + APIkey);
             req4.setRequestHeader('Accept', 'application/vnd.api+json');
@@ -95,13 +100,14 @@ function compare() {
             player2RoundMostKills = obj4JSON.data.attributes.gameModeStats["duo-fpp"].roundMostKills;
             player2Suicides = obj4JSON.data.attributes.gameModeStats["duo-fpp"].suicides;
             player2Top10s = obj4JSON.data.attributes.gameModeStats["duo-fpp"].top10s;
-            player2LongestKill = obj4JSON.data.attributes.gameModeStats["duo-fpp"].longestKill;
+            player2LongestKillString = obj4JSON.data.attributes.gameModeStats["duo-fpp"].longestKill;
+            player2LongestKill = parseFloat(player2LongestKillString).toFixed(2);
             player2Teamkills = obj4JSON.data.attributes.gameModeStats["duo-fpp"].teamkills;
             
             //Calculate winratio
             player2Wins = obj4JSON.data.attributes.gameModeStats["duo-fpp"].wins;
             player2Rounds = obj4JSON.data.attributes.gameModeStats["duo-fpp"].roundsPlayed;
-            player2WinRatio = (player2Wins / player2Rounds) * 100;
+            player2WinRatio = ((player2Wins / player2Rounds) * 100).toFixed(2);
             
             //Put variables into storage
             sessionStorage.setItem("player2Kills", player2Kills);
@@ -113,7 +119,7 @@ function compare() {
             sessionStorage.setItem("player2Teamkills", player2Teamkills);
             sessionStorage.setItem("player2WinRatio", player2WinRatio);
             
-            alert("P1 Winrate: " + player1WinRatio + " P2 Winrate: " + player2WinRatio);
+            window.location.replace("comparison.html");
         }
     }
     
